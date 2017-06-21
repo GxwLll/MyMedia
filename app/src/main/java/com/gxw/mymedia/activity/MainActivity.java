2350cb185f3bc05985565e67541c14521d1f4ca9
gengxiaowei <644875733@qq.com>
2017-06-21 16:07:17 +0800

    sub

--- app/src/main/java/com/gxw/mymedia/activity/MainActivity.java
+++ app/src/main/java/com/gxw/mymedia/activity/MainActivity.java
@@ -66,6 +66,9 @@ public class MainActivity extends AppCompatActivity
                         Log.i("GG",msg.toString());
                         Log.i("GG",msg.toString());
                         Log.i("GG",msg.toString());
+                        Log.i("GG",msg.toString());
+                        Log.i("GG",msg.toString());
+                        Log.i("GG",msg.toString());
 
 
                     }
